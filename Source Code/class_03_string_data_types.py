f82b65d31fcf8e7da9dad9fbf1bb6b78d7d95e68
tajinstituteofit <taj.instit@gmail.com>
2024-11-23 16:32:18 +0500

    Add files via upload

---
+++ Source Code/class_03_string_data_types.py
@@ -0,0 +1,117 @@
+# a = "Qasim"
+
+# b = 10
+
+
+# c = 'The name of student is "ali" '
+# print(type(c))
+
+
+# d = """ALi
+# saddam
+# musa
+# yasir"""
+# print(d)
+
+# f = 20
+# print(type(f))
+
+# g = 10
+# h = 10
+
+# z = g == h
+# print(z)
+
+
+# a = 20.5
+# print(type(a))
+
+# name = "qasim"
+
+# name1 = name.upper()
+# print(name1)
+
+# student = name.lower()
+# print(student)
+
+# student1 = name.title()
+# print(student1)
+
+# name = 10
+# name1 = 10
+
+
+# students = name == name1
+# print(students)
+
+
+
+# name = 10
+# name1 = 10
+# students = name != name1
+# print(students)
+
+
+# name = 10
+# name1 = 10
+
+# z = name < name1
+# print(z)
+
+
+
+
+# name = 12
+# name1 = 10
+
+# z = name > name1
+# print(z)
+
+
+# name = 10
+# name1 = 10
+
+# z = name <= name1
+# print(z)
+
+# name = 8
+# name1 = 10
+
+# z = name >= name1
+# print(z)
+
+# num = 10
+# num += 5
+# print(num)
+
+# num = 10
+# num -= 5
+# print(num)
+
+# num = 10
+# num **= 8
+# print(num)
+
+
+# name = 50
+# age = 20
+
+# if name != age:
+#     print("Number 20 is less then age")
+
+# if (name > age and age < name):
+#     print("name is less than age")
+
+# if (name > age or age < name):
+#     print("name is less than age")
+
+
+name = False
+
+print(not name)
+
+
+
+
+
+
