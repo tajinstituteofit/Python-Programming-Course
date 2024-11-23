45030ab4ca78ee16bbb974dcf9d7e0833b6d01e8
tajinstituteofit <taj.instit@gmail.com>
2024-11-23 19:22:59 +0500

    Add files via upload

---
+++ Source Code/class_05.py
@@ -0,0 +1,58 @@
+# students = ["ali", "ahmed", "arab", "usama", "Qasim", "talha", "ali", "ahmed", "arab", "usama", "Qasim", "talha", "ali", "ahmed", "arab", "usama", "Qasim", "talha"]
+
+# for x in students:
+
+#     print(f"The name of students is: {x.upper()}")
+#     print("All students are good")
+#     print("hello")
+
+
+
+
+# students = ["ali", "ahmed", "arab", "usama", "Qasim", "talha", "ali", "ahmed", "arab", "usama", "Qasim", "talha", "ali", "ahmed", "arab", "usama", "Qasim", "talha"]
+
+# for x in students:
+#     print(f"\nThe name of students is: {x.upper()}\n")
+    
+# num = [1,2,3,4,5,6,7,8,9,10]
+# for i in num:
+#     print(i)
+
+# for ahmed in range(1, 101):
+#     print(ahmed)
+
+
+
+# name = list(range(1, 10))
+# print(name)
+
+
+
+# name = list(range(1, 20, 3))
+# print(name)
+
+
+
+
+# students = []
+
+# for qasim in range(1, 20):
+#     result = qasim ** 2
+#     students.append(result)
+
+# print(students)
+
+
+# numbers = [0,1,2,3,4,5,6,7]
+# # print(min(numbers))
+# print(max(numbers))
+
+
+# result = [name * 2 for name in range(1, 25)]
+# print(result)
+
+# Slicing
+
+students = ["ali", "ahmed", "arab", "usama", "Qasim", "talha", "ali", "ahmed", "arab", "usama", "Qasim", "talha", "ali", "ahmed", "arab", "usama", "Qasim", "talha"]
+
+print(students[3: 5])
