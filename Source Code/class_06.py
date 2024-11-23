ff87946770c5e7e443b34b93fa864972482f587b
tajinstituteofit <taj.instit@gmail.com>
2024-11-24 01:45:23 +0500

    Add files via upload

---
+++ Source Code/class_06.py
@@ -0,0 +1,53 @@
+# players = ["ahmed", "arslan","usama", "ali","yasir", "ahmed", "arslan","usama", "ali","yasir"]
+
+# print(players[0: 3])
+
+# print(players[2: 4])
+
+# print(players[2: ])
+
+# print(players[: 3])
+
+# print(players[-2: ])
+
+# print(players[: -2])
+
+# for result in players[:3]:
+#     print(result.title())
+
+
+# result = players[:]
+# print(result)
+
+# players = ["ahmed", "arslan","usama", "ali","yasir", "ahmed", "arslan","usama", "ali","yasir"]
+# print(type(players))
+
+# result = players[:]
+# print(players)
+# players.append('saddam')
+# print(players)
+
+
+
+# Tuple
+
+
+# result = (20, 50)
+
+# print(result[0])
+# print(result[1])
+
+
+
+# result = (20, 50)
+# print(result)
+
+# result[0] = 60
+# print(result)
+
+result = (40, 60)
+# print(result)
+
+for i in result:
+
+    print(f"\n\tThe values of tuple is: {i}\n")
