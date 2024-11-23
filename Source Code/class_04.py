772041786423fc285abf67138e4ebaca73a89fc7
tajinstituteofit <taj.instit@gmail.com>
2024-11-23 18:15:24 +0500

    Add files via upload

---
+++ Source Code/class_04.py
@@ -0,0 +1,68 @@
+# names = ["ahmed", "usama", "ali", "yasir"]
+# print(names)
+
+# print("=======================================================")
+
+# print(names[1].title())
+# print(names[2].upper())
+
+# print(names[-2])
+
+# print(f"The name of student is : {names[1].title()}")
+
+# print(names)
+# names[-1] = "talha"
+# print(names)
+
+
+# print(names)
+# names.append("zubair")
+# # names.append("")
+# # names.append("")
+# print(names)
+
+# print(names)
+# names.insert(1, "mateen")
+# print(names)
+
+
+# del names[2]
+
+# print(names)
+
+
+
+# result = names.pop()
+# print(f"I have deleted the name of student and the student is: {result.upper()}")
+# print(names)
+
+# result = names.pop(1)
+# print(f"I have deleted the name with specfic index {result}")
+# print(names)
+
+# students = names.remove("ali")
+# print(students)
+# print(names)
+
+
+# names = ["ahmed", "usama", "ali", "yasir","ahmed", "usama", "ali", "yasir", "ahmed", "usama", "ali", "yasir", "ahmed", "usama", "ali", "yasir"]
+
+# names.sort()
+# print(names)
+
+# names.reverse()
+# print(names)
+
+# x = len(names)
+# print(x)
+
+# For Loop
+
+# for i in names:
+#     print(f"The name of each student is: {i}")
+#     print("Hello dear")
+#     print("Yes I am here")
+
+
+# squares = [value**2 for value in range(1, 11)]
+# print(squares)
