17a790ae65d85e4cb8641e740a69d53353d579da
tajinstituteofit <taj.instit@gmail.com>
2024-11-24 19:28:55 +0500

    Add files via upload

---
+++ Source Code/class_08_if_else.py
@@ -0,0 +1,103 @@
+# requested_toppings = ['mushrooms', 'onions', 'pineapple']
+
+# xyz = 'mushrooms' in requested_toppings
+# print(f"The response of list is: {xyz}")
+
+
+# banned_users = ['andrew', 'carolina', 'david']
+# user = 'marie'
+# if user not in banned_users:
+#     print(f"{user.title()}, you can post a response if you wish.")
+# else:
+#     print(f"{user.title()} is not in the list")
+
+
+# age = 19
+# if age >= 18:
+#     print("You are old enough to vote!")
+
+
+# age = 19
+# if age >= 18:
+#     print("You are old enough to vote!")
+#     print("Have you registered to vote yet?")
+    
+
+# age = 20
+# if age >= 18:
+#     print("You are old enough to vote!")
+#     print("Have you registered to vote yet?")
+# else:
+#     print("Sorry, you are too young to vote.")
+#     print("Please register to vote as soon as you turn 18!")
+
+
+# age = 12
+# if age < 14:
+#     print("Your admission cost is $0.")
+# elif age < 18:
+#     print("Your admission cost is $25.")
+# else:
+#     print("Your admission cost is $40.")
+
+
+# age = 12
+# if age > 4:
+#     price = 0
+# elif age < 18:
+#     price = 25
+# else:
+#     price = 40
+# print(f"Your admission cost is ${price}.")
+
+
+
+# age = 12
+# if age < 4:
+#     price = 0
+# elif age < 18:
+#     price = 25
+# elif age < 65:
+#     price = 40
+# else:
+#     price = 20
+# print(f"Your admission cost is ${price}.")
+
+
+
+# age = 12
+# if age < 4:
+#     price = 0
+# elif age < 18:
+#     price = 25
+# elif age < 65:
+#     price = 40
+# elif age >= 65:
+#     price = 20
+# print(f"Your admission cost is ${price}.")
+
+
+# requested_toppings = ['mushrooms', 'extra cheese']
+# if 'mushrooms' in requested_toppings:
+#     print("Adding mushrooms.")
+# if 'pepperoni' in requested_toppings:
+#     print("Adding pepperoni.")
+# if 'extra cheese' in requested_toppings:
+#     print("Adding extra cheese.")
+# print("\nFinished making your pizza!")
+
+
+# requested_toppings = ['mushrooms', 'extra cheese']
+# if 'mushrooms' in requested_toppings:
+#     print("Adding mushrooms.")
+# elif 'pepperoni' in requested_toppings:
+#     print("Adding pepperoni.")
+# elif 'extra cheese' in requested_toppings:
+#     print("Adding extra cheese.")
+# print("\nFinished making your pizza!")
+
+
+
+
+
+
