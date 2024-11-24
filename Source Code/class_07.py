133d47970c94acfec1183ac3aafc7254fcc0c3c4
tajinstituteofit <taj.instit@gmail.com>
2024-11-24 19:28:28 +0500

    Add files via upload

---
+++ Source Code/class_07.py
@@ -0,0 +1,68 @@
+# result = (50, 60)
+# print("Oringal values")
+
+# for j in result:
+#     print(f"Orignal values : {j}")
+
+# result = (100, 200)
+
+# print("Modified values")
+
+# for j in result:
+#     print(f"\nModified values : {j}")
+#     print(j)
+
+
+
+# if/else statment
+
+
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+
+for i in cars:
+    
+    if i == 'audi':
+        print(f"My favourite car is: {i}")
+    else:
+        print(i.title())
+
+
+# abc = 'Apple'
+# if abc != 'mushrooms':
+#     print("My favourite item is not there")
+
+
+# age = 20
+
+# if age <= 18:
+#     print("You are perfect for next step")
+# elif age <= 40:
+#     print("You are not apply for this job.")
+# elif age >= 60:
+#     print("You are now old man")
+
+# else:
+#     print("Data is not found in calss_07.py file")
+
+
+
+# answer = 17
+# if answer != 42:
+#     print("That is not the correct answer. Please try again!")
+
+
+# age_0 = int(input("Give the one values "))
+# age_1 = 18
+# x = age_0 >= 25 and age_1 >= 17
+# print(x)
+
+
+# age_0 = int(input("Give the one values "))
+# age_1 = 18
+# x = age_0 >= 25 or age_1 >= 17
+# print(x)
+
+
+# age_1 = 22
+# y = age_1
+# print(y)
